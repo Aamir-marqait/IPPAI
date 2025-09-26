c71ba3f597df0d03cda7f1c58a5dc27af635854e
md-anas-sabah <mdanassabah@gmail.com>
2025-09-27 01:10:20 +0530

    Fixing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./hero-section";
+import ThreeDecadesSection from "./ThreeDecadesSection";
 
 function page() {
   return (
     <div>
       <HeroSection />
+      <ThreeDecadesSection />
     </div>
   );
 }
