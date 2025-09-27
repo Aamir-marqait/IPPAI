b906eb484eaa1ad81fc2a7f19448db7267aa0c69
md-anas-sabah <mdanassabah@gmail.com>
2025-09-27 17:38:15 +0530

    fixing

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./hero-section";
 import EventsPage from "./our-event";
+import WhyJoinEvents from "./WhyJoinEvents";
 
 function page() {
   return (
     <div>
       <HeroSection />
       <EventsPage />
+      <WhyJoinEvents />
     </div>
   );
 }
