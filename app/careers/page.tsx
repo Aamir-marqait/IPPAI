291f5c82c55f154d06fd883a75a3f644a9961783
md-anas-sabah <mdanassabah@gmail.com>
2025-09-29 19:27:30 +0530

    feat: added contact us page

---
+++ app/careers/page.tsx
@@ -0,0 +1,13 @@
+import ApplicationFormSection from "./form";
+import HeroSection from "./hero-section";
+
+function page() {
+  return (
+    <div>
+      <HeroSection />
+      <ApplicationFormSection />
+    </div>
+  );
+}
+
+export default page;
