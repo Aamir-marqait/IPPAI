513831c6980dee8bee4fab33ab720fa6073ee8a4
md-anas-sabah <mdanassabah@gmail.com>
2025-10-07 20:40:07 +0530

    fixing home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import PartnersCarousel from "@/components/homepage/association-partners";
-import CompanyCarousel from "@/components/homepage/collab";
+// import PartnersCarousel from "@/components/homepage/association-partners";
+// import CompanyCarousel from "@/components/homepage/collab";
 import HomeHero from "@/components/homepage/hero-section";
 import Introduction from "@/components/homepage/introduction";
 import Contributors from "@/components/homepage/key-contribution";
 import KnowledgeHub from "@/components/homepage/knowledge-hub";
-import { LatestVideos } from "@/components/homepage/latest-videos";
+// import { LatestVideos } from "@/components/homepage/latest-videos";
 import LeadershipSection from "@/components/homepage/LeadershipSection";
 import Commitments from "@/components/homepage/our-commitments";
 import Testimonials from "@/components/homepage/testimonials";
@@ -15,17 +15,17 @@ export default function Home() {
   return (
     <div>
       <HomeHero />
-      <PartnersCarousel />
+      {/* <PartnersCarousel /> */}
       <Introduction />
       <KnowledgeHub />
       <Commitments />
-      <LatestVideos />
+      {/* <LatestVideos /> */}
       <VoicesInAction />
       <WhatWeDo />
       <Testimonials />
       <Contributors />
       <LeadershipSection />
-      <CompanyCarousel />
+      {/* <CompanyCarousel /> */}
     </div>
   );
 }
