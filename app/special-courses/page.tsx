4f5b058c53d5ed3dad348b585e48f8a8bcf5671b
md-anas-sabah <mdanassabah@gmail.com>
2025-10-29 23:37:43 +0530

    fixing

--- app/special-courses/page.tsx
+++ app/special-courses/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SpecialCoursesHero from "./hero-section";
 import FocusAreas from "./FocusAreas";
+import PastSpecialCourses from "./PastSpecialCourses";
 
 function page() {
   return (
     <div>
       <SpecialCoursesHero />
       <FocusAreas />
+      <PastSpecialCourses />
     </div>
   );
 }
