00888bf74757c9afe03b51a1c99fbcfc4fd0b2a5
md-anas-sabah <mdanassabah@gmail.com>
2025-10-29 23:47:30 +0530

    fixing

--- app/special-courses/page.tsx
+++ app/special-courses/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import SpecialCoursesHero from "./hero-section";
 import FocusAreas from "./FocusAreas";
 import PastSpecialCourses from "./PastSpecialCourses";
+import AboutWorkshop from "./AboutWorkshop";
 
 function page() {
   return (
@@ -9,6 +10,7 @@ function page() {
       <SpecialCoursesHero />
       <FocusAreas />
       <PastSpecialCourses />
+      <AboutWorkshop/>
     </div>
   );
 }
