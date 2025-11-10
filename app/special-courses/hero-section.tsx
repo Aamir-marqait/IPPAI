6bd8d9c303ca6b1505c3dd3a79fa1cdbef48e9a5
md-anas-sabah <mdanassabah@gmail.com>
2025-11-10 17:10:40 +0530

    fixing

--- app/special-courses/hero-section.tsx
+++ app/special-courses/hero-section.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function SpecialCoursesHero() {
   return (
-    <section className="relative min-h-[380px] sm:min-h-[420px] lg:min-h-[430px] xl:min-h-[470px] w-full flex items-center justify-center bg-[#1c1c1c] overflow-hidden px-5 md:px-0 pt-28">
+    <section className=" relative min-h-[380px] sm:min-h-[420px] lg:min-h-[430px] xl:min-h-[470px] w-full flex items-center justify-center bg-[#1c1c1c] overflow-hidden px-5 md:px-5 lg:px-0 pt-28">
       <Image
         src="/schero.png"
         alt="Background"
@@ -27,18 +27,27 @@ export default function SpecialCoursesHero() {
             />
           </div>
 
-          <div className="text-white font-poppins font-normal xl:text-[14px] xl:leading-[27px] xl:tracking-[0.32px] mb-5 text-base">
-            IRPRI, an initiative by IPPAI, conducts specialized training programs
+          <h1 className="text-white font-red-hat-display font-bold md:text-[56px] leading-[125%] tracking-[0px] mb-4 drop-shadow-lg">
+            IRPRI — Institute for Regulatory <br className="hidden sm:block" />
+            Policy & Research in Infrastructure
+          </h1>
+          <div className="text-white font-poppins font-medium text-[28px] leading-[100%] tracking-[0px] mb-5">
+            Building Capacity, Sharing Knowledge, and Empowering India&apos;s
+            Power Sector
+          </div>
+
+          <div className="text-white font-poppins font-normal xl:text-[18px] xl:leading-[27px] xl:tracking-[0.32px] mb-5 text-sm md:text-base">
+            RPRI, an initiative by IPPAI, conducts specialized training programs
             and workshops for utilities, regulators, and industry professionals
             to strengthen understanding of policy, regulation, and market
             evolution in India&apos;s Power Sector.
           </div>
           <Link
             href="#register-now"
-            className="bg-[#D3363B] hover:bg-[#b72d2d] text-white font-work-sans font-medium text-sm sm:text-base text-center cursor-pointer px-6 sm:px-7 py-2.5 rounded-full transition mb-3 inline-block"
+            className="bg-[#D3363B] hover:bg-[#b72d2d] text-white font-work-sans font-medium xl:text-[16px] xl:leading-[100%] xl:tracking-[0%] text-center cursor-pointer px-7 py-2.5 rounded-full transition mb-3 inline-block"
             style={{ boxShadow: "0px 4px 4px 0px #D3363B4F" }}
           >
-            Learn More About RPRI
+            Learn More About IRPRI
           </Link>
         </div>
         <div className=" z-20 hidden md:flex flex-col justify-center"></div>
