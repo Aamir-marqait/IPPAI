e7388647c846dd060c4b815140c90ac37c4ec362
rohith2613 <rohiths2613@gmail.com>
2025-11-12 18:25:58 +0530

    some changes

--- app/events/our-event.tsx
+++ app/events/our-event.tsx
@@ -5,6 +5,7 @@ export default async function EventsPage() {
   // Fetch events from Sanity
   const events = await getAllEvents();
   console.log("Fetched events:", events);
+  
 
   return <EventsPageClient initialEvents={events || []} />;
 }
